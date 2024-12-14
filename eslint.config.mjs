965c5d8a07160fc552193291fe83eff72feb4567
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-12-14 22:26:36 +0100

    made ecessary updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,11 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      quotes: ["error", "double", { avoidEscape: false }],
-    },
-  },
 ];
 
 export default eslintConfig;
